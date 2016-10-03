0d0c2c08f05baf31af1057b15d16c3f6963ab80e
Carl Ruiz <carl.45.cr@gmail.com>
2016-10-04 00:02:47 +0200

    public files/folders and configuration ok

--- server/main.js
+++ server/main.js
@@ -3,8 +3,19 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(express.static('public'));
+
 app.get('/',function(req, res){
    res.status(200).send("Hello world");
+});
+
+io.on('connection', function(socket){
+   console.log('conectandome con sockets');
+   socket.emit('messages', {
+      id:1,
+      text:"Welcome",
+      author:"Carl"
+   });
 });
 
 server.listen(3000, function(){
