63d6dd658bc083fe7741fe617cbf5772d2fbc3b2
Carl Ruiz <carl.45.cr@gmail.com>
2016-10-03 23:31:37 +0200

    confiration ok

--- server/main.js
+++ server/main.js
@@ -3,6 +3,9 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.get('/',function(req, res){
+   res.status(200).send("Hello world");
+});
 
 server.listen(3000, function(){
    console.log("servidor corriendo en http://localhost:3000");
