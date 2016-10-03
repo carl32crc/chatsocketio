b765df93b130637cce74c1ba76bba6e2f1e3d889
Carl Ruiz <carl.45.cr@gmail.com>
2016-10-04 00:13:55 +0200

    first message send ok

--- public/main.js
+++ public/main.js
@@ -2,4 +2,13 @@ var socket = io.connect('http://localhost:3000', {'forceNew': true});
 
 socket.on('messages', function(data){
    console.log(data);
+   render(data);
 });
+
+function render(data){
+   var html = `<div>
+                  <strong>${data.author}</strong>:
+                  <em>${data.text}</em>
+               </div>`;
+   document.getElementById('messages').innerHTML = html;
+}
